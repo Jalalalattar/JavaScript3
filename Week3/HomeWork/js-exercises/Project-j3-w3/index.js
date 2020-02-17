be22d512c99cb197e49deeba3473d3ef5121016d
Jalal Al Attar <jalalalattar89@gmail.com>
2020-02-17 22:24:59 +0100

    Project-j3-w3 Jalal Al Attar

---
+++ Week3/HomeWork/js-exercises/Project-j3-w3/index.js
@@ -0,0 +1,126 @@
+'use strict';
+{
+  
+      //  window.onload = () => main(HYF_REPOS_URL_ERROR); // to get ERROR
+  //  const HYF_REPOS_URL_ERROR ='https://api.github.com/orgsX/HackYourFuture/repos?per_page=100';
+
+  window.onload = () => main(HYF_REPOS_URL);
+  const HYF_REPOS_URL ='https://api.github.com/orgs/HackYourFuture/repos?per_page=100';
+
+
+  function createAndAppend(name, parent, options = {}) {
+    const el = document.createElement(name);
+    parent.appendChild(el);
+    Object.entries(options).forEach(([key, value]) => {
+        if (key === 'text') {
+            el.textContent = value;
+        } else {
+            el.setAttribute(key, value);
+        }
+  });
+  return el;
+  }
+
+  function renderRepoDetails(repo, table) {
+    const titles = ['Repository:', 'Description:', 'Forks:', 'Updated:'];
+    const keys = ['name', 'description', 'forks_count', 'updated_at'];
+    for (let i = 0; i < titles.length; i++) {
+      const tr = createAndAppend('tr', table);
+      createAndAppend('th', tr, { text: titles[i] });
+      if (i === 0) {
+        const td = createAndAppend('td', tr);
+        createAndAppend('a', td, {
+          href: repo.html_url,
+          text: repo.name,
+          target: 'blank_',
+        });
+      } else {
+        createAndAppend('td', tr, { text: repo[keys[i]] });
+      }
+    }
+  }
+  function renderContributors(url, container) {
+    fetch(url)
+      .then(res => res.json())
+      .then(contributors => {
+        contributors.forEach(contributor => {
+          const divCont = createAndAppend('div', container, {
+            class: 'userContributors',
+          });
+          const img = createAndAppend('img', divCont);
+          img.src = contributor.avatar_url;
+
+          const pName = createAndAppend('p', divCont);
+
+          createAndAppend('a', pName, {
+            text: contributor.login,
+            href: contributor.html_url,
+            target:'_blank',
+          })
+          const pContributes = createAndAppend('p', divCont, {
+            class: 'contributes',
+          });
+
+          createAndAppend('span', pContributes, {
+            text:contributor.contributions
+          });
+        });
+      })
+      .catch(err => {
+        createAndAppend('div', container, {
+          text: err.message,
+          class: 'alert-error',
+        });
+    });
+  }
+
+  function renderContents(repo, table, contributorsContainer) {
+    table.innerText = '';
+    contributorsContainer.innerText = '';
+
+    createAndAppend('h2', contributorsContainer, {
+      text: 'Contributors',
+    });
+
+    const divContributors = createAndAppend('div', contributorsContainer, {
+      id: 'contTable',
+    });
+
+    renderRepoDetails(repo, table);
+    renderContributors(repo.contributors_url, divContributors);
+  }
+
+  function main(url) {
+    const root = document.getElementById('root');
+    const reposContainer = document.getElementById('repos');
+    const contributorsContainer = document.getElementById('contributors');
+    const select = document.querySelector('#repos-select');
+    const table = createAndAppend('table', reposContainer);
+
+    fetch(url)
+      .then(res => res.json())
+      .then(repos => {
+        repos
+          .sort((a, b) => {
+            return a.name.localeCompare(b.name);
+          })
+          .forEach((repo, index) => {
+            createAndAppend('option', select, {
+              value: index,
+              text: repo.name,
+            });
+          });
+
+        select.addEventListener('change', () => {
+          renderContents(repos[select.value], table, contributorsContainer);
+        });
+      })
+      .catch(err => {
+        createAndAppend('p', root, {
+          text: err.message,
+          class: 'alert-error',
+        });
+      });
+  }
+
+}
